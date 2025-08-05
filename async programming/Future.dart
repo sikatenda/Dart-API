68f7167d2d63df355fadf8427f0d5b893d6b26ae
Jacques Feruzi <jferuzi019@gmail.com>
2025-08-05 14:02:23 +0200

    first commit

---
+++ async programming/Future.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('Hello, Dart!');
+
+  int age = 30;
+  String name = 'Alice';
+  //bool isDeveloper = true;
+
+  print('==========');
+  print('');
+  print('');
+  print('$name is a developer and is $age years old.');
+}
